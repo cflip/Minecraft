8189362cf7a48a88e9d70f9be873f9742e9b9e89
ComponentFlip <36554078+ComponentFlip@users.noreply.github.com>
2020-03-20 12:05:55 -0600

    Temporarily remove collision

--- src/ca/compflip/minecraft/Player.java
+++ src/ca/compflip/minecraft/Player.java
@@ -7,14 +7,9 @@ import ca.compflip.minecraft.entity.Entity;
 import ca.compflip.minecraft.level.Level;
 
 public class Player extends Entity {
-	private static final float MOVE_SPEED = 0.3f;
-	private static final float TURN_SPEED = 0.1f;
-	private static final float JUMP_POWER = 1.1f;
-
+	private static final float MOVE_SPEED = 0.02f;
+	private static final float MOUSE_SENSITIVITY = 0.1f;
 	private static final float DRAG = 0.5f;
-	private static final float GRAVITY = 0.005f;
-
-	private boolean onGround = false;
 
 	public Player(Level level) {
 		super(level);
@@ -22,8 +17,8 @@ public class Player extends Entity {
 
 	public void tick(float deltaTime) {
 		if (Mouse.isGrabbed()) {
-			rotation.x += Mouse.getDX() * TURN_SPEED;
-			rotation.y -= Mouse.getDY() * TURN_SPEED;
+			rotation.x += Mouse.getDX() * MOUSE_SENSITIVITY;
+			rotation.y -= Mouse.getDY() * MOUSE_SENSITIVITY;
 
 			if (rotation.y > 90)
 				rotation.y = 90;
@@ -47,36 +42,16 @@ public class Player extends Entity {
 				velocity.z += Math.cos(Math.toRadians(rotation.x - 90)) * MOVE_SPEED;
 			}
 
-			if (Keyboard.isKeyDown(Keyboard.KEY_SPACE) && onGround)
-				velocity.y += JUMP_POWER;
-			if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT))
-				velocity.y -= MOVE_SPEED;
+			if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) velocity.y += MOVE_SPEED;
+			if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) velocity.y -= MOVE_SPEED;
 		}
 
 		velocity.x *= DRAG;
+		velocity.y *= DRAG;
 		velocity.z *= DRAG;
 
-		velocity.y -= GRAVITY;
-
-		if (canPass(velocity.x, 0, 0)) position.x += velocity.x * deltaTime;
-		if (canPass(0, 0, velocity.z)) position.z += velocity.z * deltaTime;
-
-		if (canPass(0, velocity.y, 0)) {
-			position.y += velocity.y * deltaTime;
-			onGround = false;
-		} else {
-			velocity.y = 0;
-			onGround = true;
-		}
-	}
-
-	private boolean canPass(float xa, float ya, float za) {
-		int xx = (int) (position.x + xa);
-		int yy = (int) (position.y + ya);
-		int zz = (int) (position.z + za);
-
-		if (level.solid(xx, yy, zz))
-			return false;
-		return true;
+		position.x += velocity.x * deltaTime;
+		position.y += velocity.y * deltaTime;
+		position.z += velocity.z * deltaTime;
 	}
 }
